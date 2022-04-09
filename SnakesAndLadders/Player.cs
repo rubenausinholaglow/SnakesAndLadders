60998f721f15780036ae4fecf117fb585f3938e6
Marc Florit Vidal <mflorit@outlook.com>
2022-04-09 23:16:58 +0200

    US1: Token Can Move Across the Board

---
+++ SnakesAndLadders/Player.cs
@@ -0,0 +1,20 @@
+﻿namespace SnakesAndLadders
+{
+    public class Player
+    {
+        public string Name { get; set; }
+        public int Position { get; set; }
+
+        public Player(string name)
+        {
+            Name = name;
+            Position = 1;
+        }
+
+        public int MovePlayer(int movement)
+        {
+            return Position += movement;
+        }
+    }
+
+}
